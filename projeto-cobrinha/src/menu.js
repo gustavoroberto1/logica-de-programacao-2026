3aa3ae9745ecc8bd162ae2d5b4035343bf28b2a5
Carlos Roberto <silva@edu.sc.senai.br>
2026-08-06 21:47:33 -0300

    feat

---
+++ projeto-cobrinha/src/menu.js
@@ -0,0 +1,26 @@
+import leia from 'readline-sync';
+import { DIFICULDADES } from './config/config.js';
+import { jogo } from './objetos/jogo.js';
+
+function mostrarMenu() {
+    console.log("=============================");
+    console.log("       JOGO DA COBRINHA      ");
+    console.log("=============================");
+
+    console.log("1 - Fácil");
+    console.log("2 - Médio");
+    console.log("3 - Difícil");
+
+    var opcao = leia.questionInt("Escolha uma opcao: ");
+    var difilculdade = DIFICULDADES[opcao]
+
+    if (difilculdade === undefined) {
+        console.log("Opção Inválida");
+        return;
+    }
+
+    jogo.iniciar(difilculdade)
+}
+
+mostrarMenu();
+
